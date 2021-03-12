9219cd8c55118da95c4791cbe067d7443421502f
elinven <ivenhea@gmail.com>
2021-03-12 22:00:41 +0300

    feat: change js and json files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  return Array.isArray(matrix) == true ? matrix.reduce((arr, current, index) => index%2==0 ? arr.concat(current) : arr.concat(current.reverse()),[]) : [];
 }
